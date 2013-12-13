2d5114cb69e5203cb8058f6c8f29fb8966f8d3f3
zigang.wang <zigang.wang@hichina.com>
2013-12-13 16:47:03 +0800

    first

---
+++ my-gdx-game-desktop/src/com/me/mygdxgame/Main.java
@@ -0,0 +1,11 @@
+package com.me.mygdxgame;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+    public static void main(String[] args) {
+        new LwjglApplication(new BobBlockGame(), "my game", 480, 320, true);
+    }
+
+}
